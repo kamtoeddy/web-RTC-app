9360ece43fcd20af7312073e27520d7726162f5a
kamtoeddy <lollipopkalog@gmail.com>
2022-11-30 15:26:47 +0100

    updated auth ctx

---
+++ web-v2/src/contexts/AuthContext.tsx
@@ -0,0 +1,25 @@
+import React, { createContext, useContext } from "react";
+
+// helper modules
+import { faker } from "@faker-js/faker";
+
+type User = {
+  id: string;
+  name: string;
+};
+
+type AuthContextType = { user: User };
+
+const user = { name: faker.name.firstName(), id: faker.datatype.uuid() };
+
+const AuthContext = createContext<AuthContextType>({ user });
+
+const AuthContextProvider: React.FC<React.ReactNode> = ({ children }: any) => {
+  return (
+    <AuthContext.Provider value={{ user }}>{children}</AuthContext.Provider>
+  );
+};
+
+export default AuthContextProvider;
+
+export const useAuth = () => useContext(AuthContext);
